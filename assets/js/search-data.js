1ab34c9dcdd38c8a40ddd0ac726887611ec009b2
AlexMM28 <120156260+AlexMM28@users.noreply.github.com>
2025-08-21 02:37:40 +0000

    Deploying to gh-pages from @ AlexMM28/AlexMM28.github.io@82c3ce551a1eac8cb796e74e62495f9314d6ee77 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -504,11 +504,11 @@ ninja.data = [{
           window.open("mailto:%61%6C%65%6A%61%6D%6D%32%38@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/alejandro-maldonado-medina", "_blank");
         },
       },{
         id: 'social-rss',
@@ -516,13 +516,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
