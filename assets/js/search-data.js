34dcb12ee9a06a8b8189ff7fb590a6f764e932b0
AlexMM28 <120156260+AlexMM28@users.noreply.github.com>
2025-08-21 02:57:33 +0000

    Deploying to gh-pages from @ AlexMM28/AlexMM28.github.io@a454f4f0a079af17caed05906bef9a6b9da3ba24 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -504,6 +504,13 @@ ninja.data = [{
           window.open("mailto:%61%6C%65%6A%61%6D%6D%32%38@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/AlexMM28", "_blank");
+        },
+      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
