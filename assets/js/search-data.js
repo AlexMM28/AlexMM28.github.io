08dbc3ae3eee8b7492682158ac987bf729339582
AlexMM28 <120156260+AlexMM28@users.noreply.github.com>
2025-11-21 19:06:03 +0000

    Deploying to gh-pages from @ AlexMM28/AlexMM28.github.io@030cf53cfd1a960600e3613887c492f372631138 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,9 +409,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          section: "News",},{id: "projects-predicción-del-costo-de-seguros-médicos-con-machine-learning",
+          title: 'Predicción del costo de seguros médicos con Machine Learning.',
+          description: "Alejandro Maldonado-Medina.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
